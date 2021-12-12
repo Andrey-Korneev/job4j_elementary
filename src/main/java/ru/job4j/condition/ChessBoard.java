3688f147e80a9872c4608e433414550353233c71
andrey korneev <andrey.p.korneev@yandex.ru>
2021-12-12 13:06:03 +0300

    4.3.6. Math.abs [#373616 #239049] - исправлено

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -4,11 +4,9 @@ public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         boolean coordIsNegative = x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0;
         boolean coordIsLarge = x1 > 7 || x2 > 7 || y1 > 7 || y2 > 7;
-        if (coordIsNegative || coordIsLarge) {
-            return 0;
-        }
         int deltaX = Math.abs(x2 - x1);
-        if (deltaX == Math.abs(y2 - y1)) {
+        boolean moveIsCorrect = deltaX == Math.abs(y2 - y1);
+        if (!coordIsNegative && !coordIsLarge && moveIsCorrect) {
             return deltaX;
         }
         return 0;
