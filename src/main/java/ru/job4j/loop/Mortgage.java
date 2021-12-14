4e95c01d608f1a44267cad52f431dbf8b55781aa
andrey korneev <andrey.p.korneev@yandex.ru>
2021-12-14 19:09:07 +0300

    5.7. Ипотека [#156315] - исправлено

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        double debt = amount + amount * percent / 100;
-        while (debt > 0) {
-            amount = debt - salary;
-            debt = amount + amount * percent / 100;
+        while ((amount = amount + amount * percent / 100) > 0) {
+            amount -= salary;
             year++;
         }
         return year;
