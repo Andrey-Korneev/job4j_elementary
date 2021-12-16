76bdc268d2ff8d09b358c77c6421bee5b66d9fab
andrey korneev <andrey.p.korneev@yandex.ru>
2021-12-16 18:31:42 +0300

    6.4. Слово начинается с ... [#41585] - исправлено

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,9 +2,6 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        if (pref.length > word.length) {
-            return false;
-        }
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
             if (pref[i] != word[i]) {
