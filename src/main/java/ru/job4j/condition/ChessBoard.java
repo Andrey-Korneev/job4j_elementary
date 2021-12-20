456a73b1bf65fa6a907d0642e72fbdf3a200340f
andrey korneev <andrey.p.korneev@yandex.ru>
2021-12-20 14:18:04 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -2,12 +2,12 @@ package ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-        boolean coordIsNegative = x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0;
-        boolean coordIsLarge = x1 > 7 || x2 > 7 || y1 > 7 || y2 > 7;
-        int deltaX = Math.abs(x2 - x1);
-        boolean moveIsCorrect = deltaX == Math.abs(y2 - y1);
-        if (!coordIsNegative && !coordIsLarge && moveIsCorrect) {
-            return deltaX;
+        boolean isNegative = x1 < 0 || x2 < 0 || y1 < 0 || y2 < 0;
+        boolean isLarge = x1 > 7 || x2 > 7 || y1 > 7 || y2 > 7;
+        int delta = Math.abs(x2 - x1);
+        boolean isCorrect = delta == Math.abs(y2 - y1);
+        if (!isNegative && !isLarge && isCorrect) {
+            return delta;
         }
         return 0;
     }
